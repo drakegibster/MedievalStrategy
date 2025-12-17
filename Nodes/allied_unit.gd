b27e76c5d7bd6f3e3d26b6940288c747dabccb24
drakegibster <drakegibster@gmail.com>
2025-12-16 21:00:11 -0800

    More unit input

--- Nodes/allied_unit.gd
+++ Nodes/allied_unit.gd
@@ -4,10 +4,6 @@ var is_selected: bool = false
 @export var speed = 200
 @onready var nav_agent = $NavigationAgent2D
 
-func _input(event):
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
-		if event.pressed:
-			is_selected = true
-			print("Unit Selected")
-	if is_selected and event.is_action_pressed("right_button"):
-		nav_agent.target_position = get_global_mouse_position()
+func _on_input_event(viewport, event, shape_idx):
+	if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		print("Unit pressed")
