7bd11179ce34cc65fb6d4f562b8da266f5fa27c4
drakegibster <drakegibster@gmail.com>
2025-12-17 19:45:38 -0800

    Update allied_unit.gd

--- Nodes/allied_unit.gd
+++ Nodes/allied_unit.gd
@@ -5,11 +5,22 @@ var is_selected: bool = false
 @onready var nav_agent = $NavigationAgent2D
 @onready var highlight = $SelectionHighlight
 
-func set_selected(is_selected: bool):
-	highlight.visible = is_selected
+func _ready() -> void:
+	pass
+
+func select_unit():
+	is_selected = true
+	highlight.visible = true
+	print("unit selected")
+
+func deselect_unit():
+	is_selected = false
+	highlight.visible = false
+	print("unit deselected")
 
 func _on_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			is_selected = true
-			print("unit sellected")
+			select_unit()
+		if event.button_index == MOUSE_BUTTON_RIGHT and event.pressed:
+			deselect_unit()
