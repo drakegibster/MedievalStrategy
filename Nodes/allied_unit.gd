b30ed23042517b888e45806b02e6215bda9d7088
drakegibster <drakegibster@gmail.com>
2025-12-17 19:02:56 -0800

    Working on highlights

--- Nodes/allied_unit.gd
+++ Nodes/allied_unit.gd
@@ -3,8 +3,13 @@ extends CharacterBody2D
 var is_selected: bool = false
 @export var speed = 200
 @onready var nav_agent = $NavigationAgent2D
+@onready var highlight = $SelectionHighlight
+
+func set_selected(is_selected: bool):
+	highlight.visible = is_selected
 
 func _on_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			print("Unit pressed")
+			is_selected = true
+			print("unit sellected")
