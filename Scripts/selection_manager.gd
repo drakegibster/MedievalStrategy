c765edb8fb2b5d2df4e143a7171df93f98db0143
drakegibster <drakegibster@gmail.com>
2026-01-03 09:03:45 -0800

    Selection succesfully debugged and working (currently)

--- Scripts/selection_manager.gd
+++ Scripts/selection_manager.gd
@@ -94,7 +94,7 @@ func get_unit_at_mouse(pos):
 	#Creates an object that can run manual intersection tests outside of the normal physics loop
 	var space_state = get_world_2d().direct_space_state
 	#Translates mouse position
-	var world_pos = get_viewport().get_canvas_transform().affine_inverse() * pos
+	var world_pos = pos
 	#Define a query that checks collision layer 2
 	var query = PhysicsPointQueryParameters2D.new()
 	query.position = world_pos
